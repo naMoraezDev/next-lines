ca79c555ae6ec7b0cbf5b105bf8c5fd679c24574
Gabriel Moraes <gabriel.moraesdasilva14@gmail.com>
2023-03-20 13:24:14 -0300

    feat: create SerchBox

--- src/pages/components/Header/view.tsx
+++ src/pages/components/Header/view.tsx
@@ -1,4 +1,5 @@
-import { Flex, Switch, Tab, TabList, Tabs, Text } from "@chakra-ui/react";
+import { Flex, Icon, Input, Switch, Text } from "@chakra-ui/react";
+import { RiSearchLine } from "react-icons/ri";
 
 export function HeaderView() {
   return (
@@ -10,42 +11,62 @@ export function HeaderView() {
       mx="auto"
       px="6"
       align="center"
+      justify="space-between"
     >
-      <Text
-        fontSize={["md", "3xl"]}
-        fontWeight="bold"
-        letterSpacing="tight"
-        w="64"
-      >
-        <Text color="green.400" as="span" ml="1">
+      <Text fontSize={["md", "3xl"]} fontWeight="bold" letterSpacing="tight">
+        <Text color="green.400" as="span">
           POA{" "}
         </Text>
         _ lines
       </Text>
 
-      <Text
-        borderBottom="50"
-        borderBottomColor="green.400"
-        borderBottomWidth={3}
-        paddingBottom={2}
-        borderRadius={3}
-        marginRight={10}
-      >
-        Home
-      </Text>
+      <Flex>
+        <Text
+          borderBottom="50"
+          borderBottomColor="green.400"
+          borderBottomWidth={3}
+          paddingBottom={2}
+          borderRadius={3}
+          marginRight={10}
+        >
+          Home
+        </Text>
 
-      <Text
-        borderBottom="50"
-        borderBottomColor="green.400"
-        borderBottomWidth={3}
-        paddingBottom={2}
-        borderRadius={3}
-        marginRight={10}
-      >
-        Itinerário
-      </Text>
+        <Text
+          borderBottom="50"
+          borderBottomColor="green.400"
+          borderBottomWidth={3}
+          paddingBottom={2}
+          borderRadius={3}
+          marginRight={10}
+        >
+          Itinerário
+        </Text>
+      </Flex>
 
-      <Switch size="lg" colorScheme={"green"} ml="auto" mr="2" />
+      <Flex
+        as="label"
+        flex="1"
+        py="2"
+        px="8"
+        maxW={400}
+        alignSelf="center"
+        color="gray.200"
+        position="relative"
+        bg="gray.200"
+        borderRadius="full"
+      >
+        <Input
+          color="gray.500"
+          variant="unstyled"
+          placeholder="Buscar na plataforma"
+          _placeholder={{ color: "gray.400" }}
+        />
+
+        <Icon as={RiSearchLine} fontSize="20" color="gray.400" />
+      </Flex>
+
+      <Switch size="lg" colorScheme={"green"} />
     </Flex>
   );
 }
