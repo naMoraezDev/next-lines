b739c3ea5b1351e6fc1a47588d9dc4405e01fecf
Gabriel Moraes <gabriel.moraesdasilva14@gmail.com>
2023-03-21 11:29:45 -0300

    refactor: move Lines page to separated component

---
+++ src/features/Lines.tsx
@@ -0,0 +1,98 @@
+import {
+  Flex,
+  Stack,
+  Select,
+  TableContainer,
+  Table,
+  Thead,
+  Tr,
+  Td,
+  Tbody,
+  IconButton,
+  Icon,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { ChangeEvent } from "react";
+import { AiOutlineArrowRight } from "react-icons/ai";
+
+type Line = {
+  id: string;
+  nome: string;
+  codigo: string;
+};
+
+type LinesProps = {
+  lines: Line[];
+  filter: string;
+};
+
+export function LinesContainer({ lines, filter }: LinesProps) {
+  const router = useRouter();
+
+  function handleSelectFilter(event: ChangeEvent<HTMLSelectElement>) {
+    const filter = event.target.value;
+    if (filter !== "buses" && filter !== "lotations") {
+      router.push(`/lines/all`);
+      return;
+    }
+
+    router.push(`/lines/filter?filter=${filter}`);
+  }
+
+  return (
+    <Flex mt={23} mb={100}>
+      <Flex maxW={1480} mx="auto">
+        <Stack>
+          <Flex alignSelf="flex-end" align="center" gap="15">
+            Filtrar por:
+            <Select
+              ringColor="green.400"
+              borderColor="green.400"
+              w={200}
+              onChange={(e) => handleSelectFilter(e)}
+              value={filter}
+            >
+              <option value="all">Todos</option>
+              <option value="buses">Ônibus</option>
+              <option value="lotations">Lotação</option>
+            </Select>
+          </Flex>
+          <TableContainer width={1080}>
+            <Table variant="striped" colorScheme="facebook">
+              <Thead>
+                <Tr>
+                  <Td>Código</Td>
+                  <Td>Nome</Td>
+                  <Td>Ação</Td>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {lines.map((line: any) => (
+                  <Tr key={line.id}>
+                    <Td>{line.codigo}</Td>
+                    <Td>{line.nome}</Td>
+                    <Td>
+                      <IconButton
+                        borderRadius="full"
+                        colorScheme="green"
+                        size="sm"
+                        aria-label="Search database"
+                        icon={
+                          <Icon
+                            as={AiOutlineArrowRight}
+                            color="gray.100"
+                            fontSize="20"
+                          />
+                        }
+                      />
+                    </Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Stack>
+      </Flex>
+    </Flex>
+  );
+}
--- src/pages/lines/[...slug].tsx
+++ src/pages/lines/[...slug].tsx
@@ -1,21 +1,6 @@
+import { LinesContainer } from "@/features/Lines";
 import { api } from "@/services/api";
-import {
-  Flex,
-  Icon,
-  IconButton,
-  Select,
-  Stack,
-  Table,
-  TableContainer,
-  Tbody,
-  Td,
-  Thead,
-  Tr,
-} from "@chakra-ui/react";
-import { GetServerSideProps, GetStaticPaths, GetStaticProps } from "next";
-import { useRouter } from "next/router";
-import { ChangeEvent, useState } from "react";
-import { AiOutlineArrowRight } from "react-icons/ai";
+import { GetServerSideProps } from "next";
 
 type Line = {
   id: string;
@@ -29,74 +14,7 @@ type LinesProps = {
 };
 
 export default function Lines({ lines, filter }: LinesProps) {
-  const router = useRouter();
-
-  function handleSelectFilter(event: ChangeEvent<HTMLSelectElement>) {
-    const filter = event.target.value;
-    if (filter !== "buses" && filter !== "lotations") {
-      router.push(`/lines/all`);
-      return;
-    }
-
-    router.push(`/lines/filter?filter=${filter}`);
-  }
-
-  return (
-    <Flex mt={23} mb={100}>
-      <Flex maxW={1480} mx="auto">
-        <Stack>
-          <Flex alignSelf="flex-end" align="center" gap="15">
-            Filtrar por:
-            <Select
-              ringColor="green.400"
-              borderColor="green.400"
-              w={200}
-              onChange={(e) => handleSelectFilter(e)}
-              value={filter}
-            >
-              <option value="all">Todos</option>
-              <option value="buses">Ônibus</option>
-              <option value="lotations">Lotação</option>
-            </Select>
-          </Flex>
-          <TableContainer width={1080}>
-            <Table variant="striped" colorScheme="facebook">
-              <Thead>
-                <Tr>
-                  <Td>Código</Td>
-                  <Td>Nome</Td>
-                  <Td>Ação</Td>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {lines.map((line: any) => (
-                  <Tr key={line.id}>
-                    <Td>{line.codigo}</Td>
-                    <Td>{line.nome}</Td>
-                    <Td>
-                      <IconButton
-                        borderRadius="full"
-                        colorScheme="green"
-                        size="sm"
-                        aria-label="Search database"
-                        icon={
-                          <Icon
-                            as={AiOutlineArrowRight}
-                            color="gray.100"
-                            fontSize="20"
-                          />
-                        }
-                      />
-                    </Td>
-                  </Tr>
-                ))}
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </Stack>
-      </Flex>
-    </Flex>
-  );
+  return <LinesContainer lines={lines} filter={filter} />;
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
