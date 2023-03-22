1c276442568c848d4472b43e701d39fec48b50d6
Gabriel Moraes <gabriel.moraesdasilva14@gmail.com>
2023-03-22 11:38:31 -0300

    refactor: move presentation text into separated component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,11 @@
-import { Button, Flex, Stack, Text } from "@chakra-ui/react";
+import { Presentation } from "@/components/Presentation";
+import { Flex } from "@chakra-ui/react";
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <Flex align="center" w="100vw" justify="center" h="80vh" gap="10">
-      <Stack gap={5}>
-        <Text fontWeight="bold" fontSize="50">
-          Consulte{" "}
-          <Text as="span" color="green.400">
-            itinerários
-          </Text>{" "}
-          <br /> completos de todas as <br />
-          <Text as="span" color="green.400">
-            linhas{" "}
-          </Text>
-          de ônibus de <br /> Porto Alegre.
-        </Text>
-        <Link href="/lines/all">
-          <Button
-            borderRadius="full"
-            colorScheme="green"
-            width={400}
-            alignSelf="center"
-          >
-            Navegar
-          </Button>
-        </Link>
-      </Stack>
+      <Presentation />
 
       <Image
         src="/images/bus-stop.svg"
