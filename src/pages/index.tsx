951dcb28d991dd773cb6117bf4b529a0104bf7bd
Gabriel Moraes <gabriel.moraesdasilva14@gmail.com>
2023-03-25 15:43:56 -0300

    feat: add dynamic head to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,26 @@
 import { Presentation } from "@/components/Presentation";
 import { Flex } from "@chakra-ui/react";
 import Image from "next/image";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <Flex align="center" w="100vw" justify="center" h="80vh" gap="10">
-      <Presentation />
+    <>
+      <Head>
+        <title>Home | lines</title>
+      </Head>
 
-      <Image
-        src="/images/bus-stop.svg"
-        alt=""
-        width={450}
-        height={600}
-        priority
-      />
-    </Flex>
+      <Flex align="center" w="100vw" justify="center" h="80vh" gap="10">
+        <Presentation />
+
+        <Image
+          src="/images/bus-stop.svg"
+          alt=""
+          width={450}
+          height={600}
+          priority
+        />
+      </Flex>
+    </>
   );
 }
