e8c250f2847d072cc3c599c46d77edb0d0e0e6e5
Gabriel Moraes <gabriel.moraesdasilva14@gmail.com>
2023-03-26 19:16:42 -0300

    fix: remove searchbox popover

--- src/components/Header/components/SearchBox.tsx
+++ src/components/Header/components/SearchBox.tsx
@@ -1,73 +1,25 @@
 import { useTheme } from "@/hooks/useTheme";
-import {
-  Flex,
-  Badge,
-  Popover,
-  PopoverTrigger,
-  Input,
-  PopoverContent,
-  PopoverBody,
-  PopoverArrow,
-  Stack,
-  Icon,
-  Text,
-} from "@chakra-ui/react";
+import { Flex, Input, Icon } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { ChangeEvent, KeyboardEvent, useRef, useState } from "react";
-import { AiOutlineClose } from "react-icons/ai";
 import { RiSearchLine } from "react-icons/ri";
 
 export function SearchBox() {
   const { isDark } = useTheme();
   const router = useRouter();
   const initialFocusRef = useRef(null);
-  const [inputFilters, setInputFilters] = useState<string[]>([]);
-  const [popFilters, setPopFilters] = useState<string[]>([
-    "ônibus",
-    "lotações",
-  ]);
   const [searchTerm, setSearchTerm] = useState("");
-
-  function handleSelectFilter(filter: string) {
-    if (!inputFilters.includes(filter)) {
-      setInputFilters((filters) => [...filters, filter]);
-      setPopFilters((filters) => filters.splice(filters.indexOf(filter), 1));
-    }
-
-    return;
-  }
-
-  function handleDeSelectFilter(filter: string) {
-    if (!popFilters.includes(filter)) {
-      setPopFilters((filters) => [...filters, filter]);
-      setInputFilters((filters) => filters.splice(filters.indexOf(filter), 1));
-    }
-  }
 
   function handleOnChangeSearchTerm(event: ChangeEvent<HTMLInputElement>) {
     setSearchTerm(event.target.value);
   }
 
   function handleSearch() {
-    const filter = inputFilters.toString() === "ônibus" ? "buses" : "lotations";
-
     if (searchTerm.length === 0) {
       return;
     }
 
-    if (inputFilters.length > 1) {
-      router.push(`/search/all?term=${searchTerm}`);
-    }
-
-    if (inputFilters.length === 0) {
-      router.push(`/search/all?term=${searchTerm}`);
-    }
-
-    if (inputFilters.length < 1) {
-      router.push(`/search/all?term=${searchTerm}`);
-    } else {
-      router.push(`/search/${filter}?term=${searchTerm}`);
-    }
+    router.push(`/search/all?term=${searchTerm}`);
   }
 
   function handleKeyDown(event: KeyboardEvent<HTMLInputElement>) {
@@ -89,80 +41,15 @@ export function SearchBox() {
       bg={isDark ? "gray.700" : "gray.200"}
       borderRadius="full"
     >
-      {inputFilters.map((filter, index) => (
-        <Badge
-          key={index}
-          lineHeight={2}
-          size="small"
-          variant="solid"
-          colorScheme="green"
-          borderRadius="full"
-          mr="3"
-        >
-          <Flex align="center" gap="1">
-            {filter}
-            <AiOutlineClose
-              onClick={() => handleDeSelectFilter(filter)}
-              cursor="pointer"
-            />
-          </Flex>
-        </Badge>
-      ))}
-
-      <Popover
-        initialFocusRef={initialFocusRef}
-        placement="bottom"
-        closeOnBlur={false}
-      >
-        <PopoverTrigger>
-          <Input
-            color="gray.500"
-            variant="unstyled"
-            placeholder="Buscar uma linha"
-            _placeholder={{ color: "gray.400" }}
-            ref={initialFocusRef}
-            onChange={(e) => handleOnChangeSearchTerm(e)}
-            onKeyDown={(e) => handleKeyDown(e)}
-          />
-        </PopoverTrigger>
-
-        {Boolean(popFilters.length) && (
-          <PopoverContent
-            color={isDark ? "gray.300" : "gray.500"}
-            bg={isDark ? "gray.700" : "gray.200"}
-            mt={2}
-            border="none"
-          >
-            <PopoverBody>
-              <PopoverArrow
-                bg={isDark ? "gray.700" : "gray.200"}
-                boxShadow="none"
-              />
-              <Flex m="3">
-                <Stack>
-                  <Text>Filtrar por:</Text>
-                  <Flex gap="3">
-                    {popFilters.map((filter, index) => (
-                      <Badge
-                        key={index}
-                        lineHeight={2}
-                        size="small"
-                        variant="solid"
-                        colorScheme="green"
-                        borderRadius="full"
-                        onClick={() => handleSelectFilter(filter)}
-                        cursor="pointer"
-                      >
-                        {filter}
-                      </Badge>
-                    ))}
-                  </Flex>
-                </Stack>
-              </Flex>
-            </PopoverBody>
-          </PopoverContent>
-        )}
-      </Popover>
+      <Input
+        color="gray.500"
+        variant="unstyled"
+        placeholder="Buscar uma linha"
+        _placeholder={{ color: "gray.400" }}
+        ref={initialFocusRef}
+        onChange={(e) => handleOnChangeSearchTerm(e)}
+        onKeyDown={(e) => handleKeyDown(e)}
+      />
 
       <Icon
         as={RiSearchLine}
